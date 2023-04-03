0620f5aa16bf3d870a80ff16a4e3ccbeb09d3d1c
albim72 <marcin.albiniak72@gmail.com>
2023-04-03 12:16:34 +0200

    Update main.py

--- DZIEN_1/MOJE_MODULY/main.py
+++ DZIEN_1/MOJE_MODULY/main.py
@@ -3,6 +3,7 @@
 from dane import nr_filii as filia
 from dane import book as ks
 from funkcje.bfunkcje import czytaj_liste, czytaj_slownik
+from klasy.cklasa import CKlasa
 
 
 print("__________ wyświetlenie bezpośrednie _______________")
@@ -12,3 +13,9 @@ print(ks)
 print("__________ wyświetlenie za pomocą funkcji _______________")
 print(czytaj_liste(filia))
 print(czytaj_slownik(ks))
+
+print("__________ wyświetlenie za pomocą obiektu _______________")
+k = CKlasa(filia,ks)
+
+k.czytaj_l()
+k.czytaj_s()
