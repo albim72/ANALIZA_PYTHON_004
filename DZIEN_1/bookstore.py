092194fc28cb06c2cf1f148f0c7567785d975381
albim72 <marcin.albiniak72@gmail.com>
2023-04-03 11:49:55 +0200

    Update bookstore.py

--- DZIEN_1/bookstore.py
+++ DZIEN_1/bookstore.py
@@ -7,16 +7,33 @@ class Book:
         self.cena = cena
         self.oprawa = "miękka"
         self.create_book()
-        
-        
+
+
     #zachowanie -> funkcje klasy ->metody
     def create_book(self):
         print("utworzono nową książkę!")
-    
-    
+
+
     def print_book(self):
         print(f"Książka {self.tytul}, autor: {self.autor}, cena: {self.cena} zł,"
               f" oprawa: {self.oprawa}")
-        
+
     def rabat(self,procent):
         return self.cena*procent/100
+
+    def setcena(self,nowacena):
+        self.cena = nowacena
+
+bk1 = Book(34,"Wiedźmin","Andrzej Sapkowski",45)
+bk1.print_book()
+print(f"rabat od ceny {bk1.cena} zł wynosi {bk1.rabat(13):.2f} zł")
+print("ZMIANA CENY")
+bk1.cena = 47.8
+bk1.print_book()
+print(f"rabat od ceny {bk1.cena} zł wynosi {bk1.rabat(13):.2f} zł")
+
+bk1.setcena(55.7)
+bk1.print_book()
+print(f"rabat od ceny {bk1.cena} zł wynosi {bk1.rabat(13):.2f} zł")
+
+
