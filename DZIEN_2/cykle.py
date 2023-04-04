0873c4be6497583ac578b8ea8d67698f57d0ad31
albim72 <marcin.albiniak72@gmail.com>
2023-04-04 08:33:31 +0200

    Create cykle.py

---
+++ DZIEN_2/cykle.py
@@ -0,0 +1,31 @@
+from cycler import cycler
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+cases = [
+    None,
+    8,
+    (30,8),
+    [16,24,30],
+    [0,-1],
+    slice(200,200,3),
+    0.1,
+    0.3,
+    (0.0,0.1),
+    (0.45,0.1)
+]
+
+colors = [
+    '#1f77b4',
+    '#ff7f0e',
+    '#2ca02c',
+    '#d62728',
+    '#9467bd',
+    '#bc564b',
+    '#e377c2',
+    '#7f7f7f',
+    '#bcbd22',
+    '#17becf',
+]
